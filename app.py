59f68d23b463b017dd50e12cf4dd7ffd6ead6365
mriyann <muhammadriyan1007@gmail.com>
2025-01-12 23:29:26 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import pickle
-df=pickle.load(open('df.pkl','rb'))
-clf=pickle.load(open('knn_clf.pkl','rb'))
 import streamlit as st
+
+df=pickle.load(open('df.pkl','rb'))
+clf=pickle.load(open('rf_clf.pkl','rb'))
+
+label_map= {'Diseased':'Diseased','Healthy':'Healthy'}
+
 st.header("DocAssist")
 st.subheader('Building Intelligent Medical Decision Support System')
 st.divider()
+
 HAEMATOCRIT=st.slider("Enter the Haematocrit of the person",min_value=10,max_value=80,value=0,step=1)
 HAEMOGLOBINS=st.slider("Enter the Haemoglobins of the person",min_value=1,max_value=40,value=0,step=1)
 ERYTHROCYTE=st.slider("Enter the Erythrocyte of the person",min_value=1,max_value=40,value=0,step=1)
@@ -13,10 +18,9 @@ THROMBOCYTE=st.number_input("Enter the Leucocyte of the person", min_value=None,
 MCH=st.slider("Enter the MCH of the person",min_value=1,max_value=60,value=0,step=1)
 MCHC=st.slider("Enter the MCHC of the person",min_value=1,max_value=60,value=0,step=1)
 MCV=st.number_input("Enter the MCV of the person", min_value=None, max_value=None)
-AGE=st.number_input("Enter the Age of the person", min_value=None, max_value=None)
-# SEX=st.radio("Enter the SEX of the person",['Male','Female'],horizontal=True )
+AGE=st.number_input("Enter the Age of the person", min_value=None, max_value=None,step=1)
 
 if st.button("PREDICT"):
     op=clf.predict([[HAEMATOCRIT,HAEMOGLOBINS,ERYTHROCYTE,LEUCOCYTE,THROMBOCYTE,MCH,MCHC,MCV,AGE]])
-    op_str = str(op[0])
+    op_str = op[0]
     st.subheader(f"Considering the symptoms the person is {op_str}")
